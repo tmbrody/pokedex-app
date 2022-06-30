086be31705f4dc972235600e032314651c651505
tmbrody <brodytaylor311@gmail.com>
2022-06-30 00:45:17 -0500

    Place data array inside IIFE to prevent global access

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,59 @@
 /* create an array to store and display data regarding some different 
 pokemon and a few details about them */
-let pokemonList = [
-  {
-    name: 'Charizard', 
-    height: 5.58,
-    weight: 199.5, 
-    types: ['fire', 'flying']
-  },
-  {
-    name: 'Caterpie', 
-    height: 1,
-    weight: 6.4, 
-    types: ['bug']
-  },
-  {
-    name: 'Nidoking', 
-    height: 4.58,
-    weight: 136.7,
-    types: ['grass', 'poison']
-  },
-  {
-    name: 'Pikachu', 
-    height: 1.33,
-    weight: 13.2, 
-    types: ['electric']
-  },
-  {
-    name: 'Zubat', 
-    height: 2.58,
-    weight: 16.5, 
-    types: ['poison', 'flying']
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Charizard', 
+      height: 5.58,
+      weight: 199.5, 
+      types: ['fire', 'flying']
+    },
+    {
+      name: 'Caterpie', 
+      height: 1,
+      weight: 6.4, 
+      types: ['bug']
+    },
+    {
+      name: 'Nidoking', 
+      height: 4.58,
+      weight: 136.7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Pikachu', 
+      height: 1.33,
+      weight: 13.2, 
+      types: ['electric']
+    },
+    {
+      name: 'Zubat', 
+      height: 2.58,
+      weight: 16.5, 
+      types: ['poison', 'flying']
+    }
+  ];
+
+  function getAll() {
+    return pokemonList;
   }
-];
+
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+
+})();
 
 // display the list of pokemon names and their heights
-pokemonList.forEach(pokemon => {
+pokemonRepository.getAll().forEach(pokemon => {
   document.write(
     `<div class="test"><strong>${pokemon.name}</strong>
-    <span class="yellowgreen">Weight: ${pokemon.weight} lb</span>`
+    <span class="green">Weight: ${pokemon.weight} lb</span>`
   );
   // modify the 'types' text based on the number of types per pokemon
   pokemon.types.length > 1 
